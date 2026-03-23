bbc2e45f0e47fe1585d646fd0b1243b8a48e0e04
Debjit7-C <cdebjit935@gmail.com>
2026-03-23 22:09:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+'use strict';
 
-import userRoutes from "./routes/user.route.js";
-import promtRoutes from "./routes/promt.route.js";
+/*!
+ * Module dependencies.
+ */
 
-dotenv.config();
+const mongodbDriver = require('./drivers/node-mongodb-native');
 
-const app = express();
-const port = process.env.PORT || 4001;
-const MONGO_URL = process.env.MONGO_URI;
+require('./driver').set(mongodbDriver);
 
-// middleware
-app.use(express.json());
-app.use(cookieParser());
+const mongoose = require('./mongoose');
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+mongoose.setDriver(mongodbDriver);
 
-// DB Connection Code Goes Here!!!!
-mongoose
-  .connect(MONGO_URL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB Connection Error: ", error));
+mongoose.Mongoose.prototype.mongo = require('mongodb');
 
-// routes
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/deepseekai", promtRoutes);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = mongoose;
